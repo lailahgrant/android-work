c93c1e6a83ee8d5028ebc75c3ba46fad99576881
lailahgrant <lailahgrant19@gmail.com>
2021-12-31 10:31:05 +0300

    done project

---
+++ app/src/main/java/com/lailah/myandroidwork/AndroidDemo.java
@@ -0,0 +1,13 @@
+package com.lailah.myandroidwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_demo);
+    }
+}
